ebbcdc2c5d6121d0a0e1faf8a079664aac7a97aa
ArtyuiCraft <87765473+ArtyuiCraft@users.noreply.github.com>
2022-06-04 21:20:07 +0200

    Create rickroll.sh

---
+++ rickroll.sh
@@ -0,0 +1,35 @@
+parrotsay Never gonna give you up
+sleep 1
+parrotsay Never gonna let you down
+sleep 1
+parrotsay Never gonna run around and desert you
+sleep 1
+parrotsay Never gonna make you cry
+sleep 1
+parrotsay Never gonna say goodbye
+sleep 1
+parrotsay Never gonna tell a lie and hurt you
+sleep 1
+parrotsay Never gonna give you up
+sleep 1
+parrotsay Never gonna let you down
+sleep 1
+parrotsay Never gonna run around and desert you
+sleep 1
+parrotsay Never gonna make you cry
+sleep 1
+parrotsay Never gonna say goodbye
+sleep 1
+parrotsay Never gonna tell a lie and hurt you
+sleep 1
+parrotsay Never gonna give you up
+sleep 1
+parrotsay Never gonna let you down
+sleep 1
+parrotsay Never gonna run around and desert you
+sleep 1
+parrotsay Never gonna make you cry
+sleep 1
+parrotsay Never gonna say goodbye
+sleep 1
+parrotsay Never gonna tell a lie and hurt you
